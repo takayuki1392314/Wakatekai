e38b3621ef769f11babd748284e1815c14daf352
takayuki1392314 <takayuki.yoshizawa.1392314@gmail.com>
2022-08-31 22:18:15 +0900

    total修正

--- Original App/TotalView.swift
+++ Original App/TotalView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct TotalView: View {
+    
+    @State var nowDate = Date()
+    @State var dateText = ""
+    private let dateFormatter = DateFormatter()
+    init() {
+        dateFormatter.dateFormat = "YYYY/MM/dd(E) \nHH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ja_jp")
+        }
+    
+    
+    @AppStorage("biz_time") var biztime = 0
+    @AppStorage("it_time") var ittime = 0
+    @AppStorage("ins_time") var instime = 0
+    @AppStorage("others_time") var otherstime = 0
+    
     var body: some View {
         NavigationView{
             VStack{
+
+                Text(dateText)
+                
                 Text("学びの時間月合計")
                 Text("２０２２年９月")
                 Text("合計時間").padding()
